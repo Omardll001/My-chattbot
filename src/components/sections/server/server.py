cac81878bd6e26925dc784f4dc3ad9b1ba414f09
Omardll001 <omar.dalal.2001@gmail.com>
2025-10-01 10:50:44 +0200

    dep

--- src/components/sections/server/server.py
+++ src/components/sections/server/server.py
@@ -292,7 +292,9 @@ def api_query():
         return jsonify({"error": str(e)}), 500
 
 
+
 if __name__ == "__main__":
     host = os.environ.get("FLASK_HOST", "0.0.0.0")
-    port = int(os.environ.get("FLASK_PORT", 5174))
+    port = int(os.environ.get("PORT", os.environ.get("FLASK_PORT", 5174)))
     app.run(host=host, port=port, debug=True)
+
